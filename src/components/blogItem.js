712e8ba2ecaec30854f650db59a6fc70fade7431
craiglobo1 <craiglobo1@gmail.com>
2021-06-02 17:35:27 +0400

    adding grid

--- src/components/blogItem.js
+++ src/components/blogItem.js
@@ -20,12 +20,10 @@ export class BlogItem extends Component {
             return <Redirect push to={this.state.link} />;
           }
         return (
-            <div className="BlogItemClick" onMouseDown ={() => this.redirect('/blogs/'+ (title === "" ? "empty" : id))}>
-                <div className="blogItem">
-                    {coverImage.files.length !== 0 ? <img className="blogCoverImg" src={coverImage.files[0].name} alt="" /> : <img className="blogCoverImg" src="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F31b610c6-3a4a-446d-9969-b1d1ae51f8c2%2FUntitled.png?table=block&id=1cf0445d-6907-45ce-a108-e7b8e93e0a51&spaceId=027e1c2b-fb40-4815-a812-e373334d7a25&width=900&userId=6f951767-13bd-40af-8a2c-628936c7ac56&cache=v2" alt="" />}
-                    <div className="blogItemText">
-                        <h1>{title}</h1>
-                    </div>
+            <div className="blogItem" onMouseDown ={() => this.redirect('/blogs/'+ (title === "" ? "empty" : id))}>
+                {coverImage.files.length !== 0 ? <img className="blogCoverImg" src={coverImage.files[0].name} alt="" /> : <img className="blogCoverImg" src="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F31b610c6-3a4a-446d-9969-b1d1ae51f8c2%2FUntitled.png?table=block&id=1cf0445d-6907-45ce-a108-e7b8e93e0a51&spaceId=027e1c2b-fb40-4815-a812-e373334d7a25&width=900&userId=6f951767-13bd-40af-8a2c-628936c7ac56&cache=v2" alt="" />}
+                <div className="blogItemText">
+                    <h1>{title}</h1>
                 </div>
             </div>
         )
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 import BlogItem from '../components/blogItem';
 import Header from '../components/header';
-
+import "../style/blog.css";
 
 class Blog extends Component {
     constructor(props)
@@ -30,7 +30,6 @@ class Blog extends Component {
         <Header />
         <img  className="loader" src="https://i.pinimg.com/originals/65/ba/48/65ba488626025cff82f091336fbf94bb.gif" alt="" />
         <div className="blogView">
-          
             {this.state.blogs.map(function(blog, index){ 
               return <BlogItem key={index} blog={blog} />
             })}
