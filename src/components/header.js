1ddb58ae911bdd62fbd9ce6edb1d33e5bc5776da
craiglobo1 <craiglobo1@gmail.com>
2021-06-04 14:01:41 +0400

    updated blog header

--- src/components/header.js
+++ src/components/header.js
@@ -7,9 +7,8 @@ class Header extends Component{
         return(
             <div className="header-wrapper">
                 <div className="header">
-                    <img className="logo" alt="logo" src="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F5fa6e639-cee5-4a4d-bcd6-118c1e280844%2Fsignature_(2).png?table=block&id=5b8187e8-0846-4c69-b597-71e05b99e0d6&spaceId=027e1c2b-fb40-4815-a812-e373334d7a25&width=100&userId=6f951767-13bd-40af-8a2c-628936c7ac56&cache=v2"  />
+                    <Link to="/" ><img className="logo" alt="logo" src="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F5fa6e639-cee5-4a4d-bcd6-118c1e280844%2Fsignature_(2).png?table=block&id=5b8187e8-0846-4c69-b597-71e05b99e0d6&spaceId=027e1c2b-fb40-4815-a812-e373334d7a25&width=100&userId=6f951767-13bd-40af-8a2c-628936c7ac56&cache=v2"  /></Link>
                     <ul className="topnav">
-                    <li><Link to="/" >Home</Link></li>
                     <li><Link to="/blog" >Blog</Link></li>
                     <li><Link to="/about" >About</Link></li>
                     </ul>
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -36,6 +36,25 @@ class Blog extends Component {
     return (
         <>
         <Header />
+        <div className="blog-header">
+          <h1>📝 Blog</h1>
+          <div className="tags">
+            <svg className="tag-image" viewBox="0 0 14 14">
+              <path d="M12,1.5 L2,1.5 C1.72386,1.5 1.5,1.72386 1.5,2 L1.5,12 C1.5,12.2761 1.72386,12.5 2,12.5 L12,12.5 C12.2761,12.5 12.5,12.2761 12.5,12 L12.5,2 C12.5,1.72386 12.2761,1.5 12,1.5 Z M2,0 L12,0 C13.1046,0 14,0.895431 14,2 L14,12 C14,13.1046 13.1046,14 12,14 L2,14 C0.89543,14 0,13.1046 0,12 L0,2 C0,0.89543 0.895431,0 2,0 Z M3,3 L6.5,3 L6.5,6.5 L3,6.5 L3,3 Z M7.5,3 L11,3 L11,6.5 L7.5,6.5 L7.5,3 Z M3,7.5 L6.5,7.5 L6.5,11 L3,11 L3,7.5 Z M7.5,7.5 L11,7.5 L11,11 L7.5,11 L7.5,7.5 Z"></path>
+            </svg>
+            <div>Tags</div>
+            <div className="vSymbol">
+              <svg viewBox="0 0 30 30" className="chevronDown" >
+                <polygon points="15,17.4 4.8,7 2,9.8 15,23 28,9.8 25.2,7 " ></polygon>
+              </svg>
+            </div>
+          </div>
+          <div className="search-wrapper">
+            <svg viewBox="0 0 515.558 515.558" xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 515.558 515.558" className="searchIcon">
+                  <path d="m378.344 332.78c25.37-34.645 40.545-77.2 40.545-123.333 0-115.484-93.961-209.445-209.445-209.445s-209.444 93.961-209.444 209.445 93.961 209.445 209.445 209.445c46.133 0 88.692-15.177 123.337-40.547l137.212 137.212 45.564-45.564c0-.001-137.214-137.213-137.214-137.213zm-168.899 21.667c-79.958 0-145-65.042-145-145s65.042-145 145-145 145 65.042 145 145-65.043 145-145 145z"></path>
+            </svg>
+          </div>
+        </div>
         <img  className="loader" src="https://i.pinimg.com/originals/65/ba/48/65ba488626025cff82f091336fbf94bb.gif" alt="" />
         <div className="blogView">
             {this.state.blogs.map(function(blog, index){ 
